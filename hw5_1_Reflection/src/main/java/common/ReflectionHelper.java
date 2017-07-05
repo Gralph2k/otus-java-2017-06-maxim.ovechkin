15b981a0bd54263f18535e7c4a8fd6e0d485267f
MAIL\maxim.ovechkin <maxim.ovechkin@corp.mail.ru>
2017-07-05 14:21:39 +0300

    hw_5_1 reflection rename

---
+++ hw5_1_Reflection/src/main/java/common/ReflectionHelper.java
@@ -0,0 +1,123 @@
+package common;
+
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.reflections.ReflectionUtils.getAllMethods;
+import static org.reflections.ReflectionUtils.withAnnotation;
+
+/**
+ * Created by tully.
+ */
+@SuppressWarnings("SameParameterValue")
+public class ReflectionHelper {
+    private ReflectionHelper() {
+    }
+
+    public static <T> T instantiate(Class<T> type, Object... args) {
+        try {
+            if (args.length == 0) {
+                return type.newInstance();
+            } else {
+                return type.getConstructor(toClasses(args)).newInstance(args);
+            }
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Object getFieldValue(Object object, String name) {
+        Field field = null;
+        boolean isAccessible = true;
+        try {
+            field = object.getClass().getDeclaredField(name); //getField() for public fields
+            isAccessible = field.isAccessible();
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        } finally {
+            if (field != null && !isAccessible) {
+                field.setAccessible(false);
+            }
+        }
+        return null;
+    }
+
+    public static void setFieldValue(Object object, String name, Object value) {
+        Field field = null;
+        boolean isAccessible = true;
+        try {
+            field = object.getClass().getDeclaredField(name); //getField() for public fields
+            isAccessible = field.isAccessible();
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        } finally {
+            if (field != null && !isAccessible) {
+                field.setAccessible(false);
+            }
+        }
+    }
+
+    public static Object callMethod(Object object, String name, Object... args) throws Exception {
+        Method method = null;
+        boolean isAccessible = true;
+        try {
+            method = object.getClass().getDeclaredMethod(name, toClasses(args));
+            isAccessible = method.isAccessible();
+            method.setAccessible(true);
+            return method.invoke(object, args);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            //e.printStackTrace();
+            throw e;
+        } finally {
+            if (method != null && !isAccessible) {
+                method.setAccessible(false);
+            }
+        }
+
+    }
+
+    static private Class<?>[] toClasses(Object[] args) {
+        List<Class<?>> classes = Arrays.stream(args)
+                .map(Object::getClass)
+                .collect(Collectors.toList());
+        return classes.toArray(new Class<?>[classes.size()]);
+    }
+
+    public static Set<Class<? extends Object>> getClasses(String packageName) {
+        List<ClassLoader> classLoadersList = new LinkedList<ClassLoader>();
+        classLoadersList.add(ClasspathHelper.contextClassLoader());
+        classLoadersList.add(ClasspathHelper.staticClassLoader());
+        ConfigurationBuilder configurationBuilder =
+                new ConfigurationBuilder()
+                        .setScanners(new SubTypesScanner(false /* don't exclude Object.class */), new ResourcesScanner())
+                        .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                        .filterInputsBy(new FilterBuilder().includePackage(packageName))
+                ;
+        Reflections reflections = new Reflections(configurationBuilder);
+        return reflections.getSubTypesOf(Object.class);
+    }
+
+    public static Set<Method> getMethodsAnnotatedWith(Class targetClass, Class<? extends Annotation> annotation) {
+        Set<Method> fields = getAllMethods(targetClass, withAnnotation(annotation));
+        return fields;
+    }
+}
